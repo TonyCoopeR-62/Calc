0380b329aa3b56bafecb7b3c65f96e20614829dc
TonyCoopeR-62 <jar1991@mail.ru>
2019-01-21 16:35:06 +0300

    create script for calc

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+window.addEventListener('load', function onWindowLoad() {
+    // кнопки калькулятора
+    var btns = [
+        '1', '2', '3', '+',
+        '4', '5', '6', '-',
+        '7', '8', '9', '/',
+        '0', '=', '.', 'C'
+    ];
+})
+    //получаем форму калькулятора
+    var calc = document.getElementById('calc');
+
+    //получаем поле с вычислениями
+    var input = document.getElementById('input');
